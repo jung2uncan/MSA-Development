a77fd4d5a7bbf80d6c2251c7f58bc18fa0fa3fbf
root <jung2uncan@gmail.com>
2022-02-26 07:48:03 +0000

    마이크로 서비스 실행

---
+++ 01.실습/Chapter03.MSA 분석과 설계/[구현] 마이크로서비스의 실행/cna-start/order/src/main/java/shopmall/OrderRepository.java
@@ -0,0 +1,9 @@
+package shopmall;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+//public interface OrderRepository extends JPaRepository<Order, Long> {}
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+
+
+}
