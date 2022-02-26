e42a30b9e2a08ecad74326781cd5108fcb122b3e
root <jung2uncan@gmail.com>
2022-02-26 07:39:47 +0000

    마이크로 서비스 실행

---
+++ 01.실습/Chapter03.MSA 분석과 설계/[구현] 마이크로서비스의 실행/cna-start/delivery/src/main/java/shopmall/PolicyHandler.java
@@ -0,0 +1,26 @@
+package shopmall;
+
+import shopmall.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverOrderPlaced_StartDelivery(@Payload OrderPlaced orderPlaced){
+
+        if(orderPlaced.isMe()){
+            System.out.println("##### listener StartDelivery : " + orderPlaced.toJson());
+        }
+    }
+
+}
