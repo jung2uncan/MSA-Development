50ca59dab34b821c914c44018e7eea25b313549c
jung2uncan <74287598+jung2uncan@users.noreply.github.com>
2022-03-12 21:46:45 +0900

    Delete 01.실습/Chapter04.프론트엔드를 통한 마이크로 서비스 통합 directory

--- 01.실습/Chapter04.프론트엔드를 통한 마이크로 서비스 통합/Req-Res 방식의 MSA 연동/monolith2misvc/monolith/src/main/java/com/example/template/product/ProductOptionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.template.product;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductOptionRepository extends CrudRepository<ProductOption, Long> {
-}
